95b2d1ee343026f8f69b478a8fecdc7eaa1b395a
ryoa30 <aryo.prasetyo001@binus.ac.id>
2025-10-06 09:45:14 +0700

    setup vercel backend

--- vetch-backend/src/app.js
+++ vetch-backend/src/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",  // your Next.js URL
+  origin: process.env.CORS_URL || "http://localhost:3000",  // your Next.js URL
   credentials: true,                // allow cookies
 }));
 
